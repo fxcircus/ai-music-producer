49b27f85455d80d16a4ad56c98c7fda9f1181fc4
Roy Daniel <roydanielr@gmail.com>
2023-09-14 06:37:06 -0400

    start of React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav'
 import NewProject from './components/NewProject/NewProject';
 import Experiment from './components/Experiment/Experiment';
@@ -10,6 +11,14 @@ import CurrentProject from './components/CurrentProject/CurrentProject'
 export default function App(): any  {
   const [ isLoading, setIsLoading ] = useState(false)
   const [result, setResult] = useState("");
+
+  const getFormattedDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}_${month}_${day}`;
+  }
 
   return (
     <div className="App">
@@ -26,6 +35,7 @@ export default function App(): any  {
             <NewProject setIsLoading={setIsLoading} setResult={setResult} />
           )
           )}
+        {/* <div>{result}</div> */}
         {/* <Experiment /> */}
       </div>
     </div>
