8d5f15593628bc7ea9accf95050b113cbffdda65
Roy Daniel <roydanielr@gmail.com>
2023-09-20 06:59:44 -0400

    Styling, and get prefix from returned data

--- src/pages/CurrentProject/CurrentProject.tsx
+++ src/pages/CurrentProject/CurrentProject.tsx
@@ -10,28 +10,33 @@ interface LoaderProps {
     result: string;
 }
 
-const processResults = (resStr: string): { cardTitle: string, modalText: string }[] => {
-    const cardData: { cardTitle: string, modalText: string }[] = [];
-    const sections = resStr.split(/\d+\.\s+/); 
-    sections.shift();
-
-    for (let i = 0; i < sections.length; i++) {
-        const sectionParts = sections[i].split('\n- ');
-        let cardTitle = sectionParts[0].trim();
-        if (cardTitle.endsWith(":")) {
-            cardTitle = cardTitle.slice(0, -1);
-        }
-        const modalText = sectionParts.slice(1).join('\n- ').trim().replace(/- /g, '\n');
-        
-        cardData.push({ cardTitle, modalText });
-    }
-
-    return cardData;
-}
 
 const CurrentProject: FC<LoaderProps> = ({ result }) => {
-    const [cardsArr, setCardsArr] = useState<{ cardTitle: string, modalText: string }[]>([]);
-    const [notes, setNotes] = useState<string>(''); // Define state to store the notes
+    const [ cardsArr, setCardsArr ] = useState<{ cardTitle: string, modalText: string }[]>([]);
+    const [ notes, setNotes ] = useState<string>(''); // Define state to store the notes
+    const [ prefix, setPrefix ] = useState("")
+
+    const processResults = (resStr: string): { cardTitle: string, modalText: string }[] => {
+        setPrefix(resStr.substring(0, resStr.indexOf("1. Intro:")));
+
+        const cardData: { cardTitle: string, modalText: string }[] = [];
+        const sections = resStr.split(/\d+\.\s+/); 
+        sections.shift();
+    
+        for (let i = 0; i < sections.length; i++) {
+            const sectionParts = sections[i].split('\n- ');
+            let cardTitle = sectionParts[0].trim();
+            if (cardTitle.endsWith(":")) {
+                cardTitle = cardTitle.slice(0, -1);
+            }
+            const modalText = sectionParts.slice(1).join('\n- ').trim().replace(/- /g, '\n');
+            
+            cardData.push({ cardTitle, modalText });
+        }
+    
+        return cardData;
+    }
+
     // Function to save the project with updated notes
     const saveProject = (data: { notesVal: string }) => {
         // You can perform actions to save the project with the updated notes here.
@@ -46,7 +51,7 @@ const CurrentProject: FC<LoaderProps> = ({ result }) => {
 
     return (
         <div className="current-project">
-            <p>To compose a song in the style of *artist*, follow this general structure for each song part:</p>
+            <h4>{prefix}</h4>
             <div className="cards-area">
                 {cardsArr.map((cardData, index) => (
                     <Card
