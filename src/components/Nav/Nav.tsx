9854813048c843d5b5ed9d94f179532934b4b62d
Roy Daniel <roydanielr@gmail.com>
2023-09-24 08:21:58 -0400

    menue open close login with items

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useState, useEffect } from "react";
-
+import React, { FC, useState, useEffect, useRef } from "react";
 import './Nav.css';
 import { useNavigate } from 'react-router-dom';
 import { dummyProjects } from '../../utilities/aux/dummyProjects';
@@ -9,24 +8,57 @@ interface LoaderProps {
 }
 
 const Nav: FC<LoaderProps> = ({ userName }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  const menuRef = useRef(null);
   
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   useEffect(() => {
-    console.log(dummyProjects)
-  }, [])
+    console.log(dummyProjects);
+
+    document.addEventListener("mousedown", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleDocumentClick); // Clean up the event listener when the component unmounts
+    };
+  }, []);
+
+  const handleDocumentClick = (event: MouseEvent) => {
+    // Check if the click occurred outside of the menu and not inside any menu item
+    if (menuRef.current && !(menuRef.current as HTMLElement).contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleMenuItemClick = (event: React.MouseEvent) => {
+    event.stopPropagation(); // Prevent the click event from propagating to the document
+  };
 
   return (
-    <nav>
+    <nav className='nav-container'>
       <ul className='nav-items'>
         <li key="newproject" className="nav-item" onClick={() => navigate('/')}>New</li>
-        <li key="projects" className="nav-item" onClick={() => navigate('/projects')}>Projects</li>
+        <li key="projects" className="nav-item" onClick={toggleMenu}>Projects</li>
         <li key="about" className="nav-item" onClick={() => navigate('/about')}>About</li>
         <li key="account" onClick={() => navigate('/account')}>
           <div className='circle'>{userName}</div>
         </li>
       </ul>
+      {isOpen && (
+        <ul className='menu-items' ref={menuRef}>
+          {dummyProjects.map((project, index) => (
+            <div key={index} className="menu-item" onClick={handleMenuItemClick}>
+              <p className="project-title">{project.name}</p>
+              <p className="project-date">{project.date}</p>
+            </div>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 }
 
-export default Nav
+export default Nav;
