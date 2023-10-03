5bf09d384f44f006584305ea3bf6e228483ce5eb
Roy Daniel <roydanielr@gmail.com>
2023-10-03 08:03:21 -0400

    cleanup

--- server.tsx
+++ server.tsx
@@ -2,24 +2,20 @@ require('dotenv').config()
 require('./backend/config/database');
 const projectController = require('../../controllers/api/projects');
 
-
 // Initialize express
 const express = require('express')
 const app = express()
 const port = 8000
 const router = express.Router()
 
+// Routes
 router.get("/", getProject) 
 router.get('/projects/:projectId', getProject)
-//
 router.post('/', setProject)
-//  Update
 router.put('/:projectId', updateProject)
-// delete
 router.delete('/:projectId', deleteProject)
 
-
-// start the server
+// Start server
 app.listen(port,() => {
     console.log(`listening on port ${port}`)
 })
