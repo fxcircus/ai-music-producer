3a0d6516d1ff726e0afc0ea2acb78290b4cf1c9d
Roy Daniel <roydanielr@gmail.com>
2023-10-03 08:02:05 -0400

    Routes in server.js

--- server.tsx
+++ server.tsx
@@ -1,26 +1,23 @@
 require('dotenv').config()
 require('./backend/config/database');
+const projectController = require('../../controllers/api/projects');
+
 
 // Initialize express
 const express = require('express')
 const app = express()
 const port = 8000
+const router = express.Router()
 
-// Import Model from our models dir
-// const Model = require('./models/model')
+router.get("/", getProject) 
+router.get('/projects/:projectId', getProject)
+//
+router.post('/', setProject)
+//  Update
+router.put('/:projectId', updateProject)
+// delete
+router.delete('/:projectId', deleteProject)
 
-// Add a route with MongoDB
-// app.get('/', (req, res) => {
-//     Model.find({}, (err, foundModels) => {
-//         if (err) {
-//             res.status(400).send(err)
-//         } else {
-//             res.render(`fruits/Index`, {
-//                 models: foundModels
-//             })
-//         }
-//     })
-// }
 
 // start the server
 app.listen(port,() => {
