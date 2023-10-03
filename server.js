93b44491e0c290d0d28c1f88bea8f70b89cf1441
Roy Daniel <roydanielr@gmail.com>
2023-10-03 08:16:40 -0400

    change BE files to  JS instead of TSX

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+require('./backend/config/database.js');
+const projectController = require('./backend/controllers/project');
+
+// Initialize express
+const express = require('express')
+const app = express()
+const port = 8000
+const router = express.Router()
+
+// Routes
+router.get("/",                     projectController.getProjects) 
+router.get('/projects/:projectId',  projectController.getProject)
+router.post('/',                    projectController.setProject)
+router.put('/:projectId',           projectController.updateProject)
+router.delete('/:projectId',        projectController.deleteProject)
+
+// Start server
+app.listen(port,() => {
+    console.log(`listening on port ${port}`)
+})
